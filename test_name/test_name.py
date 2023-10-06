00d8253df2b17c243b1abe7ef3e981be9ecd3243
rus play <nikolaenkoruslan011@gmail.com>
2023-10-06 14:55:24 +0300

    refresh everything works

--- test_name/test_name.py
+++ test_name/test_name.py
@@ -1,6 +1,6 @@
 import requests, json
 import pytest
 
-def status_code_test():
+def test_status_code():
     rqst = requests.get("https://api.hh.ru:443/metro/4/?locale=EN")
     assert rqst.status_code == 200
