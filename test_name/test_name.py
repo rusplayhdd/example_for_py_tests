d458dc6f78189b5a45a5e8a3bcbf39fa82791c0a
rus play <nikolaenkoruslan011@gmail.com>
2023-10-06 17:53:28 +0300

    refresh

--- test_name/test_name.py
+++ test_name/test_name.py
@@ -4,3 +4,17 @@ import pytest
 def test_status_code():
     rqst = requests.get("https://api.hh.ru:443/metro/4/?locale=EN")
     assert rqst.status_code == 200
+
+def test_body_part():
+    r = requests.get("https://api.hh.ru:443/metro/4/?locale=EN")
+    assert r.json()["name"] == "Novosibirsk"
+
+
+@pytest.mark.parametrize(("key", "value"),
+                         [("name", "Novosibirsk"),
+                          ("id", "4")])
+
+def test_json(key, value):
+    r = requests.get("https://api.hh.ru:443/metro/4/?locale=EN")
+    assert r.json()[key] == value
+
